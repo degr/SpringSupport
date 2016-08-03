ffd77781d4037fd9ecbe028f83ac18a3253f301d
degr <degradator@bk.ru>
2016-08-03 19:32:48 +0300

    initial commit

---
+++ src/main/java/org/forweb/spring/support/Config.java
@@ -0,0 +1,32 @@
+package org.forweb.spring.support;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class Config {
+
+    @Bean(name="multipartResolver")
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver out = new CommonsMultipartResolver();
+        out.setMaxUploadSize(10000000);
+        return out;
+    }
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return mapper;
+    }
+
+}
